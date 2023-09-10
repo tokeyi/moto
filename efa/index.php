06c4b2abb3edda6307825f827059dcc9b2877e3e
tokeyi <144607979+tokeyi@users.noreply.github.com>
2023-09-10 16:39:51 +0200

    Add files via upload

---
+++ efa/index.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    require 'header.php'; 
+?>
+
+        
+                    <!-- Post preview-->
+                    <div class="container px-4 px-lg-5">
+                    <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                    <div class="post-preview">
+                        <div class="d-flex mb-4"><a class="btn btn-primary text-uppercase" href="moto.php">Clickez pour commander votre moto →</a></div>
+                        <p class="post-meta">
+                            Posted by
+                            <a href="#!">Benjamin Kuzondisa</a>
+                             2023
+                        </p>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+     
+
+
+<?php
+   require 'footer.php'; 
+?>
